0a178ecdba4d838d2105f08f67e1d77fd694a97e
Pramod_Bhavikhatte <108565873+bhavikhattepramod@users.noreply.github.com>
2022-12-22 18:18:47 +0530

    Update payslip.dart

--- payslip.dart
+++ payslip.dart
@@ -4,7 +4,9 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_cached_pdfview/flutter_cached_pdfview.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:hrms/view_payslip.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:path_provider/path_provider.dart';
@@ -40,39 +42,23 @@ class _payslipState extends State<payslip> {
     final response =
         await http.get(uri.replace(queryParameters: jsonMap), headers: {
       "Cookie": session.toString(),
-      "Accept": "*/*",
-      "Accept-Encoding": "gzip, deflate, br",
-      "Connection": "keep-alive"
     });
-
-    print(response.body);
+    print("Response:"+response.body);
+    print(response.statusCode);
     if (response.statusCode == 200) {
       var det = jsonDecode(response.body);
 
       if (det['status'] == 'success') {
         var url = det['url'];
         setState(() {
-          // reason = 'Downloading...';
+          reason = 'Downloading...';
           trydown = true;
         });
-        print(url);
-        // var res = await http.get(Uri.parse(url), headers: {
-        //   "Cookie": session.toString(),
-        //   "Accept": "*/*",
-        //   "Accept-Encoding": "gzip, deflate, br",
-        //   "Connection": "keep-alive"
-        // });
-        // print(res.body);
-        // var res = await Dio().post(url,
-        //     options: Options(
-        //         headers: {"Cookie": session.toString()},
-        //         responseType: ResponseType.bytes));
-        // log(res.data.toString());
-        // var tempDir = await getTemporaryDirectory();
-        // var file = File(tempDir.path + fileName).openSync(mode: FileMode.write);
-        // file.writeFromSync(res.data);
-        // await file.close();
-        // await Pspdfkit.present(tempDir.path + fileName);
+        print("url   "+url);
+
+
+            
+      
       } else {
         reason = det['reason'];
         setState(() {
@@ -104,6 +90,7 @@ class _payslipState extends State<payslip> {
           ),
         ),
       ),
+      
       body: SafeArea(
           child: Column(
         children: [
@@ -189,8 +176,37 @@ class _payslipState extends State<payslip> {
               style: TextStyle(
                 color: Colors.white,
               ),
+            
+              ),
             ),
-          ),
+            
+            
+            SizedBox(height: 30,),
+
+            ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              onPrimary: Colors.black87,
+              minimumSize: Size(300, 60),
+              padding: EdgeInsets.all(25),
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(2)),
+              ),
+            ),
+            onPressed: (){
+              Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MyApp(),
+                    ));
+            },
+            child: Text(
+              ' Payslip',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+              
+              ),
+            ),
           if (trydown) Text(reason),
         ],
       )),
